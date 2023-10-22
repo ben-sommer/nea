6d09180d36075e118636ca4dbfd8c945ddb459b4
Ben Sommer <32485726+ben-sommer@users.noreply.github.com>
2023-10-22 23:03:13 +0100

    feat: add basic backend server

---
+++ frontend/src/app/online-multiplayer/page.tsx
@@ -0,0 +1,14 @@
+import Board from "@/components/Board";
+import Container from "@/components/Container";
+import OnlineMultiplayer from "./OnlineMultiplayer";
+
+export default function Page() {
+    return (
+        <Container className="text-center pt-8">
+            <p className="text-2xl font-semibold text-center mb-4">
+                Online Multiplayer
+            </p>
+            <OnlineMultiplayer />
+        </Container>
+    );
+}
